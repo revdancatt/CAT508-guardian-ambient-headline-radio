bf5a754084e02069eaf67f634ba1a69d7588e59c
Rev Dan Catt <revdancatt@gmail.com>
2012-04-14 23:54:11 +0100

    Moar Ambient

--- js/radioGuGu.js
+++ js/radioGuGu.js
@@ -4,12 +4,18 @@ radio = {
     currentVolume: 100,
     targetVolume: 40,
     saying: null,
+    birdsCount: 1,
 
     init: function() {
         //utils.log('ready');
+        setInterval(function() {
+            var d = new Date();
+            var v = Math.sin(d.getTime()/25000)/2+0.5;
+            $('#birds').get(0).volume=v;
+        }, 1000);
     },
 
-    fadeSound: function(soundId, targetVolume, step) {
+    fadeSound: function(targetVolume, step) {
 
         //  decrease the current volume
         if (targetVolume < radio.currentVolume) {
@@ -18,7 +24,8 @@ radio = {
             radio.currentVolume++;
         }
         
-        $('#background1').get(0).volume=(radio.currentVolume/100);
+        $('#drone').get(0).volume=(radio.currentVolume/100);
+        $('#rain').get(0).volume=(radio.currentVolume/100*0.8);
 
         //  if we have reached the targetVolume then we are done
         if (radio.currentVolume == targetVolume) {
@@ -30,14 +37,14 @@ radio = {
         //  but we have to work out when, if we wanted to drop from 100
         //  to 40 (i.e. 60 units) in 2000ms, then we shoudl call this
         //  again in 2000/60 ms
-        setTimeout(function() {radio.fadeSound(soundId, targetVolume, step);}, step);
+        setTimeout(function() {radio.fadeSound(targetVolume, step);}, step);
 
     },
 
     say: function(msg, params) {
         this.saying = msg;
         this.sayingParams = params;
-        radio.fadeSound('leftbank', 20, 2000/Math.abs(radio.currentVolume - 20));
+        radio.fadeSound(20, 2000/Math.abs(radio.currentVolume - 20));
     },
 
     fadeFinished: function() {
@@ -57,7 +64,7 @@ radio = {
     speakEnded: function() {
 
         this.saying = null;
-        radio.fadeSound('leftbank', 100, 2000/Math.abs(radio.currentVolume - 100));
+        radio.fadeSound(100, 2000/Math.abs(radio.currentVolume - 100));
 
     }
 
